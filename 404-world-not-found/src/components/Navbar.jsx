d0e703d88c97e4787f92ec22146691fcf1adf801
lain561 <mckissicethan@gmail.com>
2025-04-12 14:03:43 -0400

    initial setup

---
+++ 404-world-not-found/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full flex justify-between items-center px-12 py-8 text-white font-mono text-xl">
+      <div className="font-bold tracking-widest">
+        <Link to="/">Binary Fallout</Link>
+      </div>
+      <div className="flex space-x-6">
+        <Link to="/about" className="hover:underline">
+          About
+        </Link>
+        <Link to="/login" className="hover:underline">
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+}
--- 404-world-not-found/src/pages/Home.jsx
+++ 404-world-not-found/src/pages/Home.jsx
@@ -1,19 +1,31 @@
 // src/pages/Home.jsx
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen min-w-screen bg-white text-black">
-      <h1 className="text-4xl font-bold mb-6">🃏 404: World Not Found</h1>
-      <p className="mb-8 text-lg">Restore the world, one card at a time.</p>
-      <button
-        onClick={() => navigate('/game')}
-        className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-lg"
-      >
-        Start Game
-      </button>
+    <div className="min-h-screen min-w-screen bg-white text-black flex flex-col">
+      <Navbar />
+      <div className="flex-1 flex flex-col items-center justify-center min-w-full"> 
+        <h1 className="text-4xl font-bold mb-6">🃏 404: World Not Found</h1>
+        <p className="mb-8 text-lg">Restore the world, one card at a time.</p>
+        <div className="flex flex-row gap-6">
+            <button
+            onClick={() => navigate('/game')}
+            className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-lg"
+            >
+            Start Game
+            </button>
+            <button
+            onClick={() => navigate('/about')}
+            className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-lg"
+            >
+            Read More
+            </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
