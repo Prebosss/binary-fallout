891ebf392d338d4eeb61ed4649936ad0616f6e88
Prebosss <kennethar2004@gmail.com>
2025-04-12 17:13:29 -0400

    login & pass hash
    created login api and password hashing using werkzeug hash algorithm for security

--- 404-world-not-found/src/backend/app.py
+++ 404-world-not-found/src/backend/app.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 from dotenv import load_dotenv, find_dotenv
 from schemas.user import User
 from mongoengine import connect, Document, StringField, DateTimeField
+from werkzeug.security import generate_password_hash
 import os
 
 # Initialize Flask app
@@ -14,7 +15,8 @@ load_dotenv(dotenv_path)
 MONGO_URI = os.getenv("MONGO_URI")
 connect(host=MONGO_URI)
 
-
+def passwordHash(password):
+    return generate_password_hash(password)
 
 @app.route('/api/addUser', methods=['POST'])
 def add_user():
@@ -28,11 +30,32 @@ def add_user():
     if User.objects(username=username).first():
         return jsonify({"error": "User already exists"}), 400
 
-    user = User(username=username, password=password)
+    user = User(username=username, password=passwordHash(password))
     user.save()
 
     return jsonify({"message": "User added successfully"}), 201
 
+@app.route('/api/login', methods=['Post'])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password:
+        return jsonify({"error": "Username and password required"}), 400
+
+    user = User.objects(username=username).first()
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    if not user.passwordVerify(password):
+        return jsonify({"error": "Invalid password"}), 401
+
+    return jsonify({"message": "Login successful"}), 200
+
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
--- 404-world-not-found/src/backend/schemas/user.py
+++ 404-world-not-found/src/backend/schemas/user.py
@@ -1,12 +1,12 @@
 from mongoengine import Document, StringField, DateTimeField
+from werkzeug.security import check_password_hash
 import datetime
 
 class User(Document):
     username = StringField(required=True, unique=True)
     password = StringField(required=True)
     created = DateTimeField(default=datetime.datetime.utcnow)
-
     def passwordVerify(self, password):
-        return self.password == password
+        return check_password_hash(self.password, password)
 
 
